f117d2790d8ebafcbdc0e35786d8b36bb3f0c75c
inegg <inegg.apps@gmail.com>
2019-05-24 10:54:49 +0900

    API was created and has been added in movie component for try to test

--- src/Routes/Movie/MovieContainer.js
+++ src/Routes/Movie/MovieContainer.js
@@ -1,7 +1,41 @@
 import React from "react";
 import MoviePresenter from "./MoviePresenter";
+import { movieApi } from "api";
 
 export default class extends React.Component {
+  state = {
+    nowPlaying: null,
+    popular: null,
+    topRated: null,
+    upcoming: null,
+    error: null,
+    loading: true
+  };
+
+  async componentDidMount() {
+    try {
+      //getting nowPlaying
+      const {
+        data: { results: nowPlaying }
+      } = await movieApi.nowPlaying();
+      const {
+        data: { results: popular }
+      } = await movieApi.popular();
+      const {
+        data: { results: topRated }
+      } = await movieApi.topRated();
+      const {
+        data: { results: upcoming }
+      } = await movieApi.upcoming();
+      console.log("🙏", "nowPlaying", nowPlaying);
+      console.log("🙏", "popular", popular);
+      console.log("🙏", "top", topRated);
+      console.log("🙏", "upcoming", upcoming);
+    } catch (error) {
+      console.log("👿MovieContainer:componentDidMount()", error);
+    }
+  }
+
   render() {
     return <MoviePresenter />;
   }
---
+++ src/api.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: "11b614bba2823bb8e43cbf99857c9cef",
+    language: "en-US"
+  }
+});
+
+export const movieApi = {
+  nowPlaying: (page = 1) =>
+    api.get("movie/now_playing", {
+      params: {
+        page
+      }
+    }),
+  popular: (page = 1) =>
+    api.get("movie/popular", {
+      params: {
+        page
+      }
+    }),
+  topRated: (page = 1) =>
+    api.get("movie/top_rated", {
+      params: {
+        page
+      }
+    }),
+  upcoming: (page = 1) =>
+    api.get("movie/upcoming", {
+      params: {
+        page
+      }
+    }),
+  detail: id =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos"
+      }
+    })
+};
+
+export const tvApi = {
+  airingToday: (page = 1) =>
+    api.get("tv/airing_today", {
+      params: {
+        page
+      }
+    }),
+  popular: (page = 1) =>
+    api.get("tv/popular", {
+      params: {
+        page
+      }
+    }),
+  topRated: (page = 1) =>
+    api.get("tv/top_rated", {
+      params: {
+        page
+      }
+    }),
+  upcoming: (page = 1) =>
+    api.get("tv/upcoming", {
+      params: {
+        page
+      }
+    }),
+  detail: id =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos"
+      }
+    })
+};
